5c8378bd129337241596bd69e5bcfe52fc156bb0
mliem2k <michael.liem2k@gmail.com>
2026-07-15 13:16:01 +0800

    api: remove dead /hero/:id route (unused, called a since-removed OpenDota endpoint)

--- apps/api/src/routes/heroes.ts
+++ apps/api/src/routes/heroes.ts
@@ -1,19 +1,10 @@
-import { Elysia, t } from 'elysia'
+import { Elysia } from 'elysia'
 import type { HeroStat } from 'types'
 import { fetchCached } from '../lib/opendota'
 
 const TTL = 60 * 60 * 6 // 6 hours
 
-export const heroesPlugin = new Elysia()
-  .get('/heroes', async () => fetchCached('/heroStats', TTL) as Promise<HeroStat[]>)
-  .get(
-    '/hero/:id',
-    async ({ params }) => {
-      const [durations, timings] = await Promise.all([
-        fetchCached(`/heroes/${params.id}/durations`, TTL),
-        fetchCached(`/heroes/${params.id}/itemTimings`, TTL),
-      ])
-      return { durations, timings }
-    },
-    { params: t.Object({ id: t.String() }) },
-  )
+export const heroesPlugin = new Elysia().get(
+  '/heroes',
+  async () => fetchCached('/heroStats', TTL) as Promise<HeroStat[]>,
+)
